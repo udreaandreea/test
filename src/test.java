db44b7ff95462beb8cda6dfc4b43e152f1518f5e
udrea.andreea@yahoo.com <udrea.andree@yahoo.com>
2015-07-20 16:07:04 +0300

    nou pe branch

--- src/test.java
+++ src/test.java
@@ -5,5 +5,7 @@ public static void main()
 System.out.print("ceva");	
 System.out.print("ceva");	
 System.out.print("ceva");	System.out.print("ceva");
+System.out.print("ceva");	System.out.print("ceva");
+
 }
 }
